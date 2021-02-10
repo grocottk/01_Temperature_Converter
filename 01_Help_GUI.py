08184e1e5984c56faf856adcf5cdff920ee13e55
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-02-11 11:59:46 +1300

    Individual attempt build during Help GUI Part 4 Video
    This build is an attemt to create an individual attempt at creating the relevant code, as suggested in the Help GUI Part 4 Video.

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -33,6 +33,51 @@ class Converter:
 
     def help(self):
         print("You asked for help")
+        get_help = Help(self)
+        get_help.help_text.configure(text="Help text goes here")
+
+
+class Help:
+
+    # Set up initial function
+    def __init__(self, partner):
+
+        background = "orange"
+
+        # Disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # Sets up child window (or help box)
+        self.help_box = Toplevel(width=600, height=600,
+                                 bg=background,
+                                 pady=10)
+
+        # Set up GUI Frame
+        self.help_box.grid()
+
+        # Set up help heading (Row 0)
+        self.help_heading = Label(self.converter_frame,
+                                  text="Help",
+                                  font=("Arial", "18", "bold"),
+                                  bg=background,
+                                  padx=10, pady=10)
+        self.help_heading.grid(row=0)
+
+        # Help text (Label, Row 1)
+        self.help_text = Label(self.converter_frame,
+                                  text="Help text goes here...",
+                                  font=("Arial", "12"),
+                                  bg=background,
+                                  padx=10, pady=10)
+        self.help_text.grid(row=1)
+
+        # Dismiss button (Row 2)
+        self.help_dismiss_button = Button(self.converter_frame,
+                                          text="Dismiss",
+                                          font=("Arial", "18", "bold"),
+                                          padx=10, pady=10,
+                                          command=self.help)
+        self.help_dismiss_button.grid(row=2)
 
 # Main Routine
 if __name__ == "__main__":
