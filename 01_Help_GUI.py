84de4ae3c5de8f55cb07cbde314abf1954b7a72f
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-02-10 13:10:33 +1300

    Help GUI and end of Part 2 Video
    This build of the Help GUI is as the project is shown at the end of the Part 2 Help GUI Video.

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -1,13 +1,28 @@
 from tkinter import *
+
+from functools import partial  # To prevent unwanted windows (being created)
+
 import random
 
-
 class Converter:
+
     def __init__(self, parent):
-        print("hello world")
 
+        # Formatting variables...
+        background_color = "purple"
 
-# main routine
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (Row 0)...
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+# Main Routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Converter")
