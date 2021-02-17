f6857ccf5b5d7fa5d713c1ec7625c78fb6f48cfa
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-02-17 13:26:43 +1300

    Build at end of Converter GUI Part 2 Video
    This is the build of the project at the end of the Converter GUI (Component 2) Part 2 Video.

--- 02_Converter_GUI.py
+++ 02_Converter_GUI.py
@@ -4,87 +4,80 @@ from functools import partial  # To prevent unwanted windows (being created)
 
 import random
 
+
 class Converter:
 
-    def __init__(self):
-
-        # Formatting variables...
+    def __init__(self, parent):
+        # Formatting variables
         background_color = "purple"
 
-        # Converter Frame...
-
-        # Converter Main Screen GUI...
+        # Converter frame
         self.converter_frame = Frame(width=600, height=600, bg=background_color,
                                      pady=10)
         self.converter_frame.grid()
 
-        # Temperature Conversion Heading (Row 0)...
-        self.temp_converter_label = Label(self.converter_frame,
-                                          text="Temperature Converter",
-                                          font=("Arial", "18", "bold"),
-                                          bg=background_color,
-                                          padx=10, pady=10)
-        self.temp_converter_label.grid(row=0)
+        # Temperature converter heading (Row 0)
+        self.temp_heading_label = Label(self.converter_frame,
+                                        text="Temperature Converter",
+                                        font=("Arial", "18", "bold"),
+                                        bg=background_color,
+                                        padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
 
-        # Help Button (Row 1)
-        self.help_button = Button(self.converter_frame, text="Help",
-                                  font=("Arial", "12"),
-                                  padx=10, pady=10,
-                                  command=self.help)
-        self.help_button.grid(row=1)
+        # User Instructions (Row 1)
+        self.temp_instructions_label = Label(self.converter_frame,
+                                             text="Type in the amount to"
+                                                  " be converted and then"
+                                                  " push one of the buttons"
+                                                  " below...",
+                                             font=("Arial", "10", "italic"),
+                                             wrap=250, justify=LEFT,
+                                             bg=background_color,
+                                             padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
 
-    def help(self):
-        print("You asked for help")
-        get_help = Help(self)
-        get_help.help_text.configure(text="Help text goes here")
+        # Temperature entry box (Row 2)
+        self.to_convert_entry = Entry(self.converter_frame,
+                                      width=20,
+                                      font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
 
+        # Conversion buttons frame (Row 3)
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
 
-if __name__ == '__main__':
-    class Help:
+        self.to_c_button = Button(self.conversion_buttons_frame,
+                                  text="To Centigrade", font="Arial 10 bold",
+                                  bg="green", padx=10, pady=10)
+        self.to_c_button.grid(row=0, column=0)
 
-        # Set up initial function
-        def __init__(self, partner):
+        self.to_f_button = Button(self.conversion_buttons_frame,
+                                  text="To Fahrenheit", font="Arial 10 bold",
+                                  bg="orange", padx=10, pady=10)
+        self.to_f_button.grid(row=0, column=1)
 
-            background = "orange"
+        # Answer label (Row 4)
+        self.answer_label = Label(self.converter_frame,
+                                  text="Answer goes here...",
+                                  font=("Arial", "10", "italic"),
+                                  wrap=250, justify=CENTER,
+                                  fg="green", bg=background_color,
+                                  padx=10, pady=10)
+        self.answer_label.grid(row=4)
 
-            # Disable help button
-            partner.help_button.config(state=DISABLED)
+        # History / Help button frame (Row 5)
+        self.history_help_buttons_frame = Frame(self.converter_frame)
+        self.history_help_buttons_frame.grid(row=5, pady=10)
 
-            # Sets up child window (or help box)
-            self.help_box = Toplevel()
+        self.histoy_button = Button(self.conversion_buttons_frame,
+                                    text="History", font="Arial 10 bold",
+                                    bg="pink", padx=10, pady=10)
+        self.histoy_button.grid(row=1, column=0)
 
-            # If users press cross at the top of the window, help closes and the help button 'releasrs'
-            self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
-
-            # Set up GUI Frame
-            self.help_frame = Frame(self.help_box, bg=background)
-            self.help_frame.grid()
-
-            # Set up help heading (Row 0)
-            self.how_heading = Label(self.help_frame,
-                                     text="Help/Instructions",
-                                     font="arial 10 bold",
-                                     bg=background)
-            self.how_heading.grid(row=0)
-
-            # Help text (Label, Row 1)
-            self.help_text = Label(self.help_frame, text="",
-                                   justify=LEFT, width=40,
-                                   bg=background, wrap=250)
-            self.help_text.grid(row=1)
-
-            # Dismiss button (Row 2)
-            self.dismiss_button = Button(self.help_frame, text="Dismiss",
-                                         width =10, bg="pink", font="arial 10 bold",
-                                         command=partial(self.close_help, partner))
-            self.dismiss_button.grid(row=2, pady=10)
-
-        # Defining close_help function
-        def close_help(self, partner):
-
-            # Put help button back into a normal state..
-            partner.help_button.config(state=NORMAL)
-            self.help_box.destroy()
+        self.help_button = Button(self.conversion_buttons_frame,
+                                  text="Help", font="Arial 10 bold",
+                                  bg="yellow", padx=10, pady=10)
+        self.help_button.grid(row=1, column=1)
 
 # Main Routine
 if __name__ == "__main__":
