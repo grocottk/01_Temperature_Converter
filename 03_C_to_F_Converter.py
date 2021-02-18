67513256179c45b418d6b0106e350705fd52c4b3
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-02-19 10:50:03 +1300

    Build at beginning of C to F Converter Video
    This is a build of the C to F Converter as of the relevant Video, predominantly before the teacher's answer. This build has been made "from scratch" by Kahlil.

---
+++ 03_C_to_F_Converter.py
@@ -0,0 +1,18 @@
+
+# This piece of code is made from scratch by Kahlil,
+# ... using a Test Plan as reference
+
+# Define test_input list
+test_celsius_input = [0, 40, 100]
+
+# For loop to convert each value
+for item in test_celsius_input:
+
+    # Defining degrees_c
+    degrees_c = item
+
+    # Fahrenheit output calculated using equation
+    fahrenheit_output = (degrees_c * 9 / 5) + 32
+
+    # Output Fahrenheit output variable
+    print("{}°C is {}°F".format(degrees_c, fahrenheit_output))
