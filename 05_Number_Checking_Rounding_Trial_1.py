708311b9516443d70b28b1c7f8b91f0dd22c224a
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-02-24 12:57:20 +1300

    Early Build of Component 5 Trial 1
    This is an early build of the first trial that was suggested to be completed in the provided ePub.

---
+++ 05_Number_Checking_Rounding_Trial_1.py
@@ -0,0 +1,14 @@
+
+# Initial attempt made from Trello Board
+
+number = 0
+
+chosen_number = int(input(number))
+
+if chosen_number == True:
+
+    print(chosen_number)
+
+elif chosen_number == False:
+
+    print("Value Error")
