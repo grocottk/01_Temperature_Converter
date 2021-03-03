33caddc0bf92a8813309faeb5f0bd5650731bc99
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-03-03 20:33:00 +1300

    Build toward end of Temperature Converter Part 3 Video
    This build of the program is one that is most likely not functional, or does not function as intended. It will most likely be worked on in the future, with many of the current issues hopefully being resolved in coming builds.

--- 08_Converter.py
+++ 08_Converter.py
@@ -4,7 +4,6 @@ from functools import partial  # To prevent unwanted windows (being created)
 
 import random
 
-
 class Converter:
 
     def __init__(self, parent):
@@ -12,14 +11,14 @@ class Converter:
         background_color = "medium purple"
 
         # Converter frame
-        self.converter_frame = Frame(width=600, height=600, bg=background_color,
+        self.converter_frame = Frame(bg=background_color,
                                      pady=10)
         self.converter_frame.grid()
 
         # Temperature converter heading (Row 0)
         self.temp_heading_label = Label(self.converter_frame,
                                         text="Temperature Converter",
-                                        font=("Arial", "18", "bold"),
+                                        font=("Arial", "20", "bold"),
                                         bg=background_color,
                                         padx=10, pady=10)
         self.temp_heading_label.grid(row=0)
@@ -31,7 +30,7 @@ class Converter:
                                                   " push one of the buttons"
                                                   " below...",
                                              font=("Arial", "10", "italic"),
-                                             wrap=250, justify=LEFT,
+                                             wrap=290, justify=LEFT,
                                              bg=background_color,
                                              padx=10, pady=10)
         self.temp_instructions_label.grid(row=1)
@@ -85,26 +84,55 @@ class Converter:
 
         print(to)
 
+        error = "pink" # Shows this colour when an entry has errors
+
         # Retrieve amount entered into "Entry" field
         to_convert = self.to_convert_entry.get()
 
         # Check amount is a valid number
         try:
             to_convert = float(to_convert)
-            print("yay")
+            # has_errors = "no"
 
-        except ValueError:
-            print("oops")
+        # Check and convert to degrees Fahrenheit
+        # if low == -273 and to_convert >= low:
+            fahrenheit = (to_convert * 9/5) + 32
+            to_convert = self.round_it(to_convert)
+            fahrenheit = self.round_it(fahrenheit)
+            answer = "{} degrees celsius is {} degrees fahrenheit".format(to_convert, fahrenheit)
 
-        # Convert to degrees Fahrenheit
+        # Check and convert to degrees Celsius
+        # elif low == -459 and to_convert >= low:
+            celsius = (to_convert - 32) * 5/9
+            to_convert = self.round_it(to_convert)
+            celsius = self.round_it(celsius)
+            answer = "{} degrees fahrenheit is {} degrees celsius".format(to_convert, celsius)
 
-        # Convert to degrees Celsius
+        # If the entry is not a valid temperature
+        # else:
+            answer = "This temperature entry is too cold, and therefore not valid"
+            has_errors = "yes"
 
         # Round values
 
         # Display converted value
 
+        # If no error is present
+        # if has_errors == "no":
+            self.converted_label.configure(text = answer, fg = "blue")
+            self.to_convert_entry.configure(bg = "white")
+
+        # If an error is present
+        # else:
+            self.converted_label.configure(text="Please enter a number.", fg="red")
+            self.to_convert_entry.configure(bg=error)
+
         # Add answer to list for "History" purposes
+
+        # Print an error message is applicable
+        except ValueError:
+            self.converted_label.configure(text="Please enter a number", fg="red")
+            # self.to_convert_entry.configure(bg=error)
 
 # Main Routine
 if __name__ == "__main__":
