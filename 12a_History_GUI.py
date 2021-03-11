7351e460e6c00865cd25380df18af491432f1b73
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-03-12 10:54:33 +1300

    Build during History GUI Part 2 Video
    This is the build during the second video in the series of "History GUI".

--- 12a_History_GUI.py
+++ 12a_History_GUI.py
@@ -4,6 +4,7 @@ from functools import partial  # To prevent unwanted windows (being created)
 
 import random
 
+
 class Converter:
 
     def __init__(self, parent):
@@ -29,65 +30,58 @@ class Converter:
                                           padx=10, pady=10)
         self.temp_converter_label.grid(row=0)
 
-        # Help Button (Row 1)
-        self.help_button = Button(self.converter_frame, text="Help",
-                                  font=("Arial", "12"),
-                                  padx=10, pady=10,
-                                  command=self.help)
-        self.help_button.grid(row=1)
+        # history Button (Row 1)
+        self.history_button = Button(self.converter_frame, text="History",
+                                     font=("Arial", "12"),
+                                     padx=10, pady=10,
+                                     command=self.history)
+        self.history_button.grid(row=1)
 
-    def help(self):
-        print("You asked for help")
-        get_help = Help(self)
-        get_help.help_text.configure(text="Help text goes here")
+    def history(self):
+        print("You asked for history")
+        get_history = History(self)
+        get_history.history_text.configure(text="History text goes here")
 
 
 if __name__ == '__main__':
-    class Help:
+    class History:
 
         # Set up initial function
         def __init__(self, partner):
 
-            background = "orange"
+            background = "green"
 
-            # Disable help button
-            partner.help_button.config(state=DISABLED)
+            # Disable history button
+            partner.history_button.config(state=DISABLED)
 
-            # Sets up child window (or help box)
-            self.help_box = Toplevel()
+            # Sets up child window (or history box)
+            self.history_box = Toplevel()
 
-            # If users press cross at the top of the window, help closes and the help button 'releasrs'
-            self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+            # If users press cross at the top of the window, history closes and the history button 'releases'
+            self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
 
             # Set up GUI Frame
-            self.help_frame = Frame(self.help_box, bg=background)
-            self.help_frame.grid()
+            self.history_frame = Frame(self.history_box, bg=background)
+            self.history_frame.grid()
 
-            # Set up help heading (Row 0)
-            self.how_heading = Label(self.help_frame,
-                                     text="Help/Instructions",
+            # Set up history heading (Row 0)
+            self.how_heading = Label(self.history_frame,
+                                     text="History/Instructions",
                                      font="arial 10 bold",
                                      bg=background)
             self.how_heading.grid(row=0)
 
-            # Help text (Label, Row 1)
-            self.help_text = Label(self.help_frame, text="",
-                                   justify=LEFT, width=40,
-                                   bg=background, wrap=250)
-            self.help_text.grid(row=1)
+            # history text (Label, Row 1)
+            self.history_text = Label(self.history_frame,
+                                      text="Here are the most recently completed calculations."
+                                           "Please use the export button to create a text file"
+                                           " of all your calculations for this session.",
+                                      font="arial 10 italic",
+                                      justify=LEFT, width=40, fg="light orange",
+                                      bg=background, wrap=250)
+            self.history_text.grid(row=1)
 
-            # Dismiss button (Row 2)
-            self.dismiss_button = Button(self.help_frame, text="Dismiss",
-                                         width =10, bg="pink", font="arial 10 bold",
-                                         command=partial(self.close_help, partner))
-            self.dismiss_button.grid(row=2, pady=10)
-
-        # Defining close_help function
-        def close_help(self, partner):
-
-            # Put help button back into a normal state..
-            partner.help_button.config(state=NORMAL)
-            self.help_box.destroy()
+            # Show user their [calculation] history (Row 2)
 
 # Main Routine
 if __name__ == "__main__":
